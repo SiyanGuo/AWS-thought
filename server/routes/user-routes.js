41060c24ff63c316c0b9e3818cd42518fae69db8
Siyan Guo <siyang@Hey-There.local>
2022-01-09 20:47:11 -0500

    add API Post route

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -57,5 +57,27 @@ dynamodb.query(params, (err, data) =>{
 });
 });
 
+//create a new thought
+router.post('/users', (req, res) =>{
+    const params = {
+        TableName: table,
+        Item:{
+            "username": req.body.username,
+            "createdAt":Date.now(),
+            "thought":req.body.thought
+        }
+    };
+
+    //database call
+    dynamodb.put(params, (err, data) =>{
+        if(err){
+            console.error("Unable to add items, Error JSON:",JSON.stringify(err, null, 2));
+            res.status(500).json(err);
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+            res.json({"Added": JSON.stringify(data, null, 2)});
+        }
+    });
+}); 
 
 module.exports = router;
