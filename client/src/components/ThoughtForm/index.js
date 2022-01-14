063a74efe4e51bb66fecba7a7ad380920e098ba1
Siyan Guo <siyang@Hey-There.local>
2022-01-14 15:39:40 -0500

    Integrate the Upload Feature into the front end

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({
@@ -6,6 +6,36 @@ const ThoughtForm = () => {
     thought: "",
   });
   const [characterCount, setCharacterCount] = useState(0);
+
+  const [isUploaded, setUpload] = useState(false)
+  const fileInput = useRef(null);
+
+  const handleImageUpload = event => {
+    event.preventDefault();
+
+    const data = new FormData();
+    data.append('image', fileInput.current.files[0]);
+    console.log('data', data)
+    //send image file to endpoint with the postImage function
+    const postImage = async () => {
+      try {
+        const res = await fetch('/api/image-upload', {
+          mode: "cors",
+          method: "POST",
+          body: data
+        })
+        if (!res.ok) throw new Error(res.statusText);
+        const postResponse = await res.json();
+        setFormState({ ...formState, image: postResponse.location })
+        setUpload(true)
+        console.log("postImage:", postResponse.Location)
+        return postResponse.Location;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postImage()
+  };
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -62,6 +92,26 @@ const ThoughtForm = () => {
           className="form-input col-12 "
           onChange={handleChange}
         ></textarea>
+
+        {/* add image upload  */}
+        <label className="form-input col-12 p-1">
+          Add an image to your thought:
+          <input
+            type="file"
+            ref={fileInput}
+            className="form-input p-2"
+          />
+            <button
+              className="btn"
+              onClick={handleImageUpload}
+              type="submit"
+            >
+            {!isUploaded ? 
+              "Upload" : "Uploaded"
+            }
+            </button>
+
+        </label>
         <button className="btn col-12 " type="submit">
           Submit
         </button>
